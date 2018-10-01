040a13d62ec8c2b253c3ff10a15713c184edb8eb
William Ma <wma1017@gmail.com>
2018-09-30 17:48:53 -0700

    Updated tests

--- modin/pandas/test/test_dataframe.py
+++ modin/pandas/test/test_dataframe.py
@@ -4,10 +4,7 @@ from __future__ import print_function
 
 import pytest
 import io
-
-import modin.pandas as pd
 import numpy as np
-from numpy.testing import assert_array_equal
 import pandas
 import pandas.util.testing as tm
 from pandas.tests.frame.common import TestData
@@ -17,176 +14,17 @@ from modin.pandas.utils import to_pandas
 from numpy.testing import assert_array_equal
 import sys
 
-from .utils import df_equals, df_is_empty, arg_keys, name_contains
+from .utils import (df_equals, df_is_empty, arg_keys, name_contains, test_dfs_keys, 
+        test_dfs_values, numeric_dfs, test_func_keys, test_func_values, 
+        query_func_keys, query_func_values, agg_func_keys, agg_func_values, 
+        numeric_agg_funcs, quantiles_keys, quantiles_values, indices_keys, 
+        indices_values, axis_keys, axis_values, bool_arg_keys, bool_arg_values,
+        bool_none_arg_keys, bool_none_arg_values, int_arg_keys, int_arg_values)
 
 # Force matplotlib to not use any Xwindows backend.
 matplotlib.use("Agg")
 
-if sys.version_info[0] < 3:
-    PY2 = True
-else:
-    PY2 = False
-
-
-# Input data and functions for the tests
-# The test data that we will test our code against
-test_data = {
-    "empty_data": {},
-    "int_data": {
-        "col1": [0, 1, 2, 3], 
-        "col2": [4, 5, 6, 7], 
-        "col3": [8, 9, 10, 11], 
-        "col4": [12, 13, 14, 15], 
-        "col5": [0, 0, 0, 0]
-        },
-    "float_data": { 
-        "col1": [0.0, 1.0, 2.0, 3.0], 
-        "col2": [4.0, 5.0, 6.0, 7.0], 
-        "col3": [8.0, 9.0, 10.0, 11.0], 
-        "col4": [12.0, 13.0, 14.0, 15.0], 
-        "col5": [0.0, 0.0, 0.0, 0.0]
-        },
-    "sparse_nan_data": {
-        "col1": [1, 2, 3, np.nan],
-        "col2": [4, 5, np.nan, 7],
-        "col3": [8, np.nan, 10, 11],
-        "col4": [np.nan, 13, 14, 15],
-    },
-    "dense_nan_data": {
-        "col1": [np.nan, 2, np.nan, 0],
-        "col2": [3, 4, np.nan, 1],
-        "col3": [np.nan, np.nan, np.nan, 5],
-        "col3": [6, np.nan, np.nan, 10],
-        },
-    "int_float_object_data": { 
-        "col1": [1, 2, 3, 4], 
-        "col2": [4, 5, 6, "7"], 
-        "col3": [8.0, 9.4, 10.1, 11.3], 
-        "col4": ["a", "b", "c", "d"]
-        },
-    "datetime_timedelta_data": {
-            'col1': [np.datetime64('2010'), np.datetime64('2011'), np.datetime64('2011-06-15T00:00'), np.datetime64('2009-01-01')],
-            'col2': [np.datetime64('2010'), np.datetime64('2011'), np.datetime64('2011-06-15T00:00'), np.datetime64('2009-01-01')],
-            'col3': [np.timedelta64(1, 'M'), np.timedelta64(2, 'D'), np.timedelta64(3, 'Y'), np.timedelta64(20, 'D')],
-            'col4': [np.timedelta64(1, 'M'), np.timedelta64(2, 'D'), np.timedelta64(3, 'Y'), np.timedelta64(20, 'D')],
-            },
-    "all_data": { 
-            'col1' : 1.,
-            'col2' : np.datetime64('2011-06-15T00:00'),
-            'col3' : np.array([3] * 4,dtype='int32'),
-            'col4' : 'foo',
-            'col5' : True
-            }
-    }
-
-test_dfs = {name: [pd.DataFrame(data), pandas.DataFrame(data)] for name, data in test_data.items()}
-test_dfs_keys = list(test_dfs.keys())
-test_dfs_values = list(test_dfs.values())
-
-numeric_dfs = ["empty_data", "int_data", "float_data", "sparse_nan_data", "dense_nan_data"]
-
-# Test functionss for applymap
-test_func = {
-        "plus one": lambda x: x + 1, 
-        "convert to string": lambda x: str(x), 
-        "square": lambda x: x * x, 
-        "identity": lambda x: x, 
-        "return false": lambda x: False 
-        }
-test_func_keys = list(test_func.keys())
-test_func_values = list(test_func.values())
-
-numeric_test_funcs = ["plus one", "square"]
-
-# Test functions for query
-query_func = { 
-        "col1 < col2": "col1 < col2",
-        "col3 > col4": "col3 > col4",
-        "col1 == col2": "col1 == col2",
-        "(col2 > col1) and (col1 < col3)": "(col2 > col1) and (col1 < col3)" 
-        }
-query_func_keys = list(query_func.keys())
-query_func_values = list(query_func.values())
-
-# Test agg functions for apply, agg, and aggregate
-agg_func = {
-        "sum": "sum", 
-        "df sum": lambda df: df.sum(), 
-        "sum mean": ["sum", "mean"], 
-        "sum sum": ["sum", "sum"],
-        "sum df sum": ["sum", lambda df: df.sum()]
-        }
-agg_func_keys = list(agg_func.keys())
-agg_func_values = list(agg_func.values())
-
-numeric_agg_funcs = ["sum mean", "sum sum", "sum df sum"]
-
-# Test q values for quantiles
-quantiles = {
-        "0.25": 0.25, 
-        "0.5": 0.5, 
-        "0.75": 0.75, 
-        "0.66": 0.66, 
-        "0.01": 0.01,
-        "list": [0.25, 0.5, 0.75, 0.66, 0.01]
-        }
-quantiles_keys = list(quantiles.keys())
-quantiles_values = list(quantiles.values())
-
-# Test indices for get, set_index, __contains__, insert
-indices = {
-        "col1": "col1",
-        "col2": "col2",
-        "A": "A",
-        "B": "B",
-        "does not exist": "does not exist"
-        }
-indices_keys = list(indices.keys())
-indices_values = list(indices.values())
-
-# END Test input data and functions
-
-
-# Parametrizations of common kwargs
-axis = {
-        "over rows": 0, 
-        "over rows": "rows", 
-        "over columns": 1, 
-        "over columns": "columns"
-        }
-axis_keys = list(axis.keys())
-axis_values = list(axis.values())
-
-bool_arg = {
-        "True": True,
-        "False": False
-        }
-bool_arg_keys = list(bool_arg.keys())
-bool_arg_values = list(bool_arg.values())
-
-bool_none_arg = {
-        "True": True,
-        "False": False,
-        "None": None
-        }
-bool_none_arg_keys = list(bool_none_arg.keys())
-bool_none_arg_values = list(bool_none_arg.values())
-
-int_arg = {
-        '-5': -5, 
-        '-1': -1, 
-        '0': 0, 
-        '1': 1, 
-        '5': 5
-        }
-int_arg_keys = list(int_arg.keys())
-int_arg_values = list(int_arg.values())
-
-# END parametrizations of common kwargs
-
-
 # Test inter df math functions
-@pytest.fixture
 def inter_df_math_helper(op):
     frame_data = {
         "col1": [0, 1, 2, 3],
@@ -2593,8 +2431,14 @@ def test_stack(ray_df, pandas_df):
 
 
 @pytest.mark.parametrize("ray_df, pandas_df", test_dfs_values, ids=test_dfs_keys)
-def test_std(ray_df, pandas_df):
-    assert df_equals(ray_df.std(), pandas_df.std())
+@pytest.mark.parametrize("axis", axis_values, ids=axis_keys)
+@pytest.mark.parametrize("skipna", bool_arg_values, ids=arg_keys("skipna", bool_arg_keys))
+@pytest.mark.parametrize("numeric_only", bool_none_arg_values, ids=arg_keys("numeric_only", bool_none_arg_keys))
+@pytest.mark.parametrize("ddof", int_arg_values, ids=arg_keys("ddof", int_arg_keys))
+def test_std(ray_df, pandas_df, axis, skipna, numeric_only, ddof):
+    ray_result = ray_df.std(axis=axis, skipna=skipna, numeric_only=numeric_only, ddof=ddof)
+    pandas_result = pandas_df.std(axis=axis, skipna=skipna, numeric_only=numeric_only, ddof=ddof)
+    assert df_equals(ray_result, pandas_result)
 
 
 @pytest.mark.skip(reason="Defaulting to Pandas")
@@ -2605,8 +2449,18 @@ def test_style(ray_df, pandas_df):
 
 
 @pytest.mark.parametrize("ray_df, pandas_df", test_dfs_values, ids=test_dfs_keys)
-def test_sum(ray_df, pandas_df):
-    assert df_equals(ray_df.sum(), pandas_df.sum())
+@pytest.mark.parametrize("axis", axis_values, ids=axis_keys)
+@pytest.mark.parametrize("skipna", bool_arg_values, ids=arg_keys("skipna", bool_arg_keys))
+@pytest.mark.parametrize("numeric_only", bool_arg_values, ids=arg_keys("numeric_only", bool_arg_keys))
+@pytest.mark.parametrize("min_count", int_arg_values, ids=arg_keys("min_count", int_arg_keys))
+def test_sum(request, ray_df, pandas_df, axis, skipna, numeric_only, min_count):
+    if numeric_only or name_contains(request.node.name, numeric_dfs):
+        ray_result = ray_df.sum(axis=axis, skipna=skipna, numeric_only=numeric_only, min_count=min_count)
+        pandas_result = pandas_df.sum(axis=axis, skipna=skipna, numeric_only=numeric_only, min_count=min_count)
+        assert df_equals(ray_result, pandas_result)
+    else:
+        with pytest.raises(TypeError):
+            ray_df.sum(axis=axis, skipna=skipna, numeric_only=numeric_only, min_count=min_count)
 
 
 @pytest.mark.skip(reason="Defaulting to Pandas")
